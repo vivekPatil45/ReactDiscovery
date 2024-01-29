a2473d1615b767abe7266e0f93903757ad12914c
vivekPatil45 <vivekrp4503@gmail.com>
2024-01-29 16:16:57 +0530

    todo project

---
+++ 10todocontextLocal/src/context/TodoContext.js
@@ -0,0 +1,23 @@
+import React,{useContext} from 'react';
+
+export const TodoContext = React.createContext({
+    todos : [
+        {
+            id:1,
+            todo :"Todo msg",
+            completed : false
+        }
+    ],
+    addTodo : (todo)=>{},
+    updatedTodo : (id,todo)=>{},
+    deleteTodo : (id)=>{},
+    toggleComplete : (id)=>{}
+
+})
+
+export const useTodo = ()=>{
+    return useContext(TodoContext)
+}
+
+
+export const TodoProvider = TodoContext.Provider
